8777982984a490b479d6b19defdfb7154cd9db66
vakapallimahesh <vakapallimahesh48@gmail.com>
2026-03-13 21:47:58 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, reverse = 0, rem;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    while(n != 0) {
+        rem = n % 10;
+        reverse = reverse * 10 + rem;
+        n = n / 10;
+    }
+
+    printf("Reverse = %d", reverse);
+
+    return 0;
+}
