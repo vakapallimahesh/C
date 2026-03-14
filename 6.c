bf352f73b5342e8137cc85372acd95d4facf437b
vakapallimahesh <vakapallimahesh48@gmail.com>
2026-03-14 19:42:59 +0530

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num, temp, rem, sum = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    while(temp != 0) {
+        rem = temp % 10;
+        sum = sum + rem * rem * rem;
+        temp = temp / 10;
+    }
+
+    if(sum == num)
+        printf("Armstrong Number");
+    else
+        printf("Not Armstrong");
+
+    return 0;
+}
